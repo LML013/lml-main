6a94c82831207df260b6a27c74b790fee47a5b38
Luis Miguel Luna <LuisMiguelLuna@gmail.com>
2023-02-02 14:37:38 -0700

    Projects Page
    Figured out the projects page, whoo!

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -1,11 +1,40 @@
 //TODO: Make this work
 
-// import type { PageServerLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
-// export const load = (async () => {
-// 	//get projects from database
+export const load = (async () => {
+	//get projects from database
+    //currently temp static data
+    const projects = {"projects":[
+            {
+                name: 'LuisMiguelLuna.com',
+                desc: 'This website. Currently being built with HTML, CSS, JS, and Svelte. Deployed using GitHub and Netlify.',
+                link: 'https://luismiguelluna.com',
+                github: 'https://github.com/LML013/lml-main'
+            },
 
-// 	//TODO: Turn this into a Github GraphQL API call for Pinned repos
+            {
+                name: 'Green Saber Games',
+                desc: 'Tabletop gaming blog & content hub made with Wordpress.',
+                link: 'https://greensabergames.com',
+                github: ''
+            },
+            {
+                name: 'Phoenix Squadron',
+                desc: 'Splash page for information about playing Star Wars X-wing in the Phoenix Area',
+                link: 'https://phoenixsquadronaz.com',
+                github: 'https://github.com/LML013/phoenix-nest'
+            },
+            {
+                name: 'Loruki',
+                desc: "An implementation of Brad Traversy's Build a Responsive Website | HTML, CSS Grid, Flexbox & More tutorial video. Deployed using GitHub and Netlify.",
+                link: 'https://luismiguelluna.com',
+                github: 'https://github.com/LML013/loruki-website'
+            }
+        ]
+    };
 
-// 	return projects;
-// }) satisfies PageServerLoad;
+	//TODO: Turn this into a Github GraphQL API call for Pinned repos
+
+	return projects;
+}) satisfies PageServerLoad;
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,33 +3,6 @@
 	import Project from './Project.svelte';
 
 	export let data: PageData;
-
-	const projects = [
-		{
-			name: 'LuisMiguelLuna.com',
-			desc: 'This website. Currently being built with HTML, CSS, JS, and Svelte. Deployed using GitHub and Netlify.',
-			link: 'https://luismiguelluna.com',
-			github: 'https://github.com/LML013/lml-main'
-		},
-
-		{
-			name: 'Green Saber Games',
-			desc: 'Tabletop gaming blog & content hub made with Wordpress.',
-			link: 'https://greensabergames.com'
-		},
-		{
-			name: 'Phoenix Squadron',
-			desc: 'Splash page for information about playing Star Wars X-wing in the Phoenix Area',
-			link: 'https://phoenixsquadronaz.com',
-			github: 'https://github.com/LML013/phoenix-nest'
-		},
-		{
-			name: 'Loruki',
-			desc: "An implementation of Brad Traversy's Build a Responsive Website | HTML, CSS Grid, Flexbox & More tutorial video. Deployed using GitHub and Netlify.",
-			link: 'https://luismiguelluna.com',
-			github: 'https://github.com/LML013/loruki-website'
-		}
-	];
 </script>
 
 <svelte:head>
@@ -37,8 +10,6 @@
 	<meta name="description" content="Luis Miguel Luna's Projects" />
 </svelte:head>
 
-<section>
-	{#each projects as project}
-		<Project {...project} />
-	{/each}
-</section>
+{#each data.projects as project}
+	<Project {...project} />
+{/each}
