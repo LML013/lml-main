a2a1ea5501c1aa5a46ee239eb306e55f7fd9e516
Luis Miguel Luna <LuisMiguelLuna@gmail.com>
2023-02-03 11:50:19 -0700

    Projects export declaration
    Tried to make projects page a little more DRY. Kinda worked.

--- src/routes/projects/Project.svelte
+++ src/routes/projects/Project.svelte
@@ -1,12 +1,12 @@
 <script>
-	export let name = '';
-	export let desc = '';
-	export let link = '';
-	export let github = '';
+	export let name = '',
+		desc = '',
+		link = '',
+		github = '';
 </script>
 
 <h2><a href={link}>{name}</a></h2>
 <p>{desc}</p>
 {#if github}
-<a href={github}><i class="fa-brands fa-github"></i></a>
+	<a href={github}><i class="fa-brands fa-github" /></a>
 {/if}
